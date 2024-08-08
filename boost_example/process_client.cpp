2377ee9afedad91ddcf0b97ef465c07ede38517c
zerorains <linjunlu@zerorains.top>
2024-08-08 14:14:01 +0800

    feat: remove the useless code

--- boost_example/process_client.cpp
+++ boost_example/process_client.cpp
@@ -19,41 +19,14 @@
 
 int main()
 {
-  /* version 1
-  boost::interprocess::shared_memory_object::remove("IPC_CHENELL");
-  boost::interprocess::managed_shared_memory managed_shm(boost::interprocess::create_only, "IPC_CHENELL", 1024);
-  typedef boost::interprocess::allocator<char, boost::interprocess::managed_shared_memory::segment_manager> CharAllocator;
-  typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> string;
-  string *s = managed_shm.construct<string>("CODE")("Hello!", managed_shm.get_segment_manager());
-  s->insert(5, ", world");
-
-  // sync
-  boost::interprocess::interprocess_mutex *mtx = managed_shm.construct<boost::interprocess::interprocess_mutex>("mtx")();
-  boost::interprocess::interprocess_condition *cnd = managed_shm.construct<boost::interprocess::interprocess_condition>("cnd")();
-  boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(*mtx);
-
-  int i = 10;
-  while (i--)
-  {
-    std::cout << "[Client] send : " << *s << std::endl;
-    cnd->notify_all();
-    cnd->wait(lock);
-
-    std::pair<string *, size_t> p = managed_shm.find<string>("CODE");
-    std::cout << "[Client] get  : " << *p.first << std::endl;
-    std::cout<<"\n";
-  }
-  cnd->notify_all();
-  boost::interprocess::shared_memory_object::remove("IPC_CHENELL");
-
-  */
-
-  /* */
+  // create shared memory and lock
   SharedMemoryManagerTMP manager("MySharedMemory");
   bi::interprocess_mutex *mtx = manager.segment.construct<bi::interprocess_mutex>("mtx")();
   bi::interprocess_condition *cnd = manager.segment.construct<bi::interprocess_condition>("cnd")();
   bi::scoped_lock<bi::interprocess_mutex> lock(*mtx);
 
+
+  // create table data
   arrow::Int64Builder builder;
   builder.AppendValues({1, 2, 3, 4, 5});
   std::shared_ptr<arrow::Array> array1;
@@ -67,6 +40,7 @@ int main()
   auto schema = arrow::schema({arrow::field("a", arrow::int64()), arrow::field("b", arrow::int64())});
   std::shared_ptr<arrow::Table> my_table = arrow::Table::Make(schema, {array1, array2});
   {
+    // transform the table to buffer
     std::shared_ptr<arrow::Buffer> buffer;
     std::shared_ptr<arrow::io::BufferOutputStream> stream = arrow::io::BufferOutputStream::Create(1024 * 1024).ValueOrDie();
     std::shared_ptr<arrow::ipc::RecordBatchWriter> writer;
@@ -83,10 +57,10 @@ int main()
       buffer = result2.ValueOrDie();
     }
 
-    // write the table
+    // write the table to share memory
     char *shm_ptr = manager.segment.construct<char>("MyTable")[buffer->size()]();
     std::memcpy(shm_ptr, buffer->data(), buffer->size());
-    std::cout << "finish write the table buffer in the shared memory\n";
+    std::cout << "[Client] finish write the table buffer in the shared memory\n";
     for (int i = 0; i < my_table->num_columns(); i++)
     {
       std::shared_ptr<arrow::ChunkedArray> column = my_table->column(i);
@@ -102,13 +76,16 @@ int main()
       std::cout << std::endl;
     }
   }
+  // wait for the server to read the table
   cnd->notify_all();
   cnd->wait(lock);
   {
-    std::cout << "OK I got it\n";
+    std::cout << "[Client] OK I got it\n";
+    // recieve the server result
     char *shm_ptr = manager.segment.find<char>("MyResult").first;
     std::size_t size = manager.segment.find<char>("MyResult").second;
 
+    // transform the buffer to table
     std::shared_ptr<arrow::Buffer> buffer = arrow::Buffer::Wrap(shm_ptr, size);
     std::shared_ptr<arrow::io::InputStream> input = std::make_shared<arrow::io::BufferReader>(buffer);
     arrow::Result<std::shared_ptr<arrow::ipc::RecordBatchReader>> result = arrow::ipc::RecordBatchStreamReader::Open(input);
@@ -119,7 +96,7 @@ int main()
       arrow::Result<arrow::RecordBatchWithMetadata> batch_result = reader->ReadNext();
       if (!batch_result.ok())
       {
-        std::cerr << "Failed to read next record batch: " << batch_result.status() << std::endl;
+        std::cerr << "[Client] Failed to read next record batch: " << batch_result.status() << std::endl;
         return 1;
       }
 
@@ -133,6 +110,9 @@ int main()
 
     arrow::Result<std::shared_ptr<arrow::Table>> table_result = arrow::Table::FromRecordBatches(reader->schema(), batches);
     my_table = *table_result;
+
+    // read the result table
+    std::cout<<"[Client] The result table is:\n";
     for (int i = 0; i < my_table->num_columns(); i++)
     {
       std::shared_ptr<arrow::ChunkedArray> column = my_table->column(i);
@@ -148,7 +128,9 @@ int main()
       std::cout << std::endl;
     }
   }
+  //  close the server
   cnd->notify_all();
+  // destroy the shared memory
   bi::shared_memory_object::remove("MySharedMemory");
   return 0;
 }
--- boost_example/process_server.cpp
+++ boost_example/process_server.cpp
@@ -16,51 +16,24 @@
 #include <arrow/io/memory.h>
 #include <arrow/table.h>
 #include <arrow/status.h>
-
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
 #include <arrow/python/pyarrow.h>
-
-namespace py = pybind11;
 
 int main()
 {
-    /* version 1
-    boost::interprocess::managed_shared_memory managed_shm(boost::interprocess::open_only, "IPC_CHENELL");
-    typedef boost::interprocess::allocator<char, boost::interprocess::managed_shared_memory::segment_manager> CharAllocator;
-    typedef boost::interprocess::basic_string<char, std::char_traits<char>, CharAllocator> string;
-
-    boost::interprocess::interprocess_mutex *mtx = managed_shm.find_or_construct<boost::interprocess::interprocess_mutex>("mtx")();
-    boost::interprocess::interprocess_condition *cnd = managed_shm.find_or_construct<boost::interprocess::interprocess_condition>("cnd")();
-    boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(*mtx);
-
-    int i = 10;
-    while (i--)
-    {
-        std::pair<string *, size_t> p = managed_shm.find<string>("CODE");
-        std::cout << "[Server] get  : " << *p.first << std::endl;
-
-        string * s = p.first;
-        std::string tmp = std::to_string(i);
-        string tmp_shm_str(tmp.begin(), tmp.end(), s->get_allocator());
-        s->insert(s->size(), tmp_shm_str);
-        std::cout << "[Server] sned : " << *s << std::endl;
-        std::cout<<"\n";
-        cnd->notify_all();
-        cnd->wait(lock);
-    }
-    */
-
+    // create shared memory and lock
     SharedMemoryManagerTMP manager("MySharedMemory");
     bi::interprocess_mutex *mtx = manager.segment.find<bi::interprocess_mutex>("mtx").first;
     bi::interprocess_condition *cnd = manager.segment.find<bi::interprocess_condition>("cnd").first;
     bi::scoped_lock<bi::interprocess_mutex> lock(*mtx);
 
+    // get the client table from the shared memory
     char *shm_ptr = manager.segment.find<char>("MyTable").first;
     std::size_t size = manager.segment.find<char>("MyTable").second;
 
+    // transform the buffer to table
     std::shared_ptr<arrow::Buffer> buffer = arrow::Buffer::Wrap(shm_ptr, size);
 
+    // read the table from the buffer
     std::shared_ptr<arrow::io::InputStream> input = std::make_shared<arrow::io::BufferReader>(buffer);
     arrow::Result<std::shared_ptr<arrow::ipc::RecordBatchReader>> result = arrow::ipc::RecordBatchStreamReader::Open(input);
 
@@ -99,47 +72,9 @@ int main()
     }
 
     std::shared_ptr<arrow::Table> my_table = *table_result;
-    std::cout << "I got the table from the shared memory 1\n";
-    /*
-    // parse the data from shared memory
-    std::shared_ptr<arrow::ChunkedArray> column1 = my_table->column(0);
-    std::shared_ptr<arrow::ChunkedArray> column2 = my_table->column(1);
+    std::cout << "[Server] I got the table from the shared memory 1\n";
 
-    // compute
-    arrow::Int64Builder sum_builder;
-    for (int chunk_idx = 0; chunk_idx < column1->num_chunks(); chunk_idx++)
-    {
-        auto chunk1 = std::static_pointer_cast<arrow::Int64Array>(column1->chunk(chunk_idx));
-        auto chunk2 = std::static_pointer_cast<arrow::Int64Array>(column2->chunk(chunk_idx));
-        for (int i = 0; i < chunk1->length(); i++)
-        {
-            sum_builder.Append(chunk1->Value(i) + chunk2->Value(i));
-        }
-    }
-    std::shared_ptr<arrow::Array> sum_array;
-    sum_builder.Finish(&sum_array);
-    std::shared_ptr<arrow::Field> sum_field = arrow::field("sum", arrow::int64());
-    std::shared_ptr<arrow::ChunkedArray> sum_column = std::make_shared<arrow::ChunkedArray>(sum_array);
-    auto result2 = my_table->AddColumn(my_table->num_columns(), sum_field, sum_column);
-    my_table = *result2;
-    std::cout << my_table->num_columns() << "    finished compute \n";
-
-    // print compute result
-    for (int i = 0; i < my_table->num_columns(); i++)
-    {
-        std::shared_ptr<arrow::ChunkedArray> column = my_table->column(i);
-        std::cout << my_table->field(i)->name() << ": ";
-        for (int chunk_idx = 0; chunk_idx < column->num_chunks(); chunk_idx++)
-        {
-            auto chunk = std::static_pointer_cast<arrow::Int64Array>(column->chunk(chunk_idx));
-            for (int j = 0; j < chunk->length(); j++)
-            {
-                std::cout << chunk->Value(j) << " ";
-            }
-        }
-        std::cout << std::endl;
-    }
-    */
+    // using python to process the table
     if (!Py_IsInitialized())
     {
         Py_Initialize();
@@ -203,7 +138,6 @@ def process_table(table):
         my_table = arrow::py::unwrap_table(py_table_tmp).ValueOrDie();
     }
 
-    std::cout << my_table->num_columns() << std::endl;
     // create new buffer
     std::shared_ptr<arrow::io::BufferOutputStream> stream = arrow::io::BufferOutputStream::Create(1024 * 1024).ValueOrDie();
     std::shared_ptr<arrow::ipc::RecordBatchWriter> writer;
@@ -222,7 +156,7 @@ def process_table(table):
 
     shm_ptr = manager.segment.construct<char>("MyResult")[buffer->size()]();
     std::memcpy(shm_ptr, buffer->data(), buffer->size());
-    std::cout << "finish write the table buffer in the shared memory\n";
+    std::cout << "[Server] finish write the table buffer in the shared memory\n";
     cnd->notify_all();
     cnd->wait(lock);
     PyGILState_Release(gstate);
